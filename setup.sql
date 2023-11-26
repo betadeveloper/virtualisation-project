89da06d6930fe40f91c03008e739c49adf902089
GOD\rtautkus <raigardas.tautkus@god.dev>
2023-11-26 16:40:54 +0200

    added playbook db backup stuff

--- setup.sql
+++ setup.sql
@@ -5,6 +5,6 @@ CREATE TABLE reservations (
   	res_status	integer
 );
 
-INSERT INTO reservations values ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Foo', 'FooStore', 2);
-INSERT INTO reservations values ('4ad4d2e2-55d6-4282-8010-7b633d6170b9', 'Bar', 'BarStore', 1);
-INSERT INTO reservations values ('5a90571e-c8ac-4984-83bd-c2b23fa3dae6', 'Baz', 'BazStore', 0);
+INSERT INTO reservations values ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Create docker', 'Docker', 2);
+INSERT INTO reservations values ('4ad4d2e2-55d6-4282-8010-7b633d6170b9', 'Virtualize systems', 'System', 1);
+INSERT INTO reservations values ('5a90571e-c8ac-4984-83bd-c2b23fa3dae6', 'Develop application', 'Application', 0);
